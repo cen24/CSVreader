354e8855026b7085e6d2633118d31ccfa6cd83bb
Chinedu Nnaji <cen24@njit.edu>
2019-10-25 02:14:53 -0400

    Feature: added division function

---
+++ src/Calculator.py
@@ -0,0 +1,42 @@
+def addition(a, b):
+    c = a + b
+    return c
+
+def subtraction(a, b):
+    c = a - b
+    return c
+
+def multiplication(a, b):
+    c = a * b
+    return c
+
+def division(a, b):
+    c = a / b
+    return c
+
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+    def multiplication(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+    def multiplication(self, a, b):
+        self.result = division(a, b)
+        return self.result
+    def multiplication(self, a, b):
+        self.result = square(a, b)
+        return self.result
+    def multiplication(self, a, b):
+        self.result = squareroot(a, b)
+        return self.result
