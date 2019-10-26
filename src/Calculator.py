344166ea8f316b45fad43622e2b196ba06e301ee
Chinedu Nnaji <52545426+cen24@users.noreply.github.com>
2019-10-25 20:37:38 -0400

    Fix: updated lines of codes.

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,49 +1,53 @@
-def addition(a, b):
+import math
+
+def add(a, b):
     c = a + b
     return c
 
-def subtraction(a, b):
+def sub(a, b):
     c = a - b
     return c
 
-def multiplication(a, b):
+def times(a, b):
     c = a * b
     return c
 
-def division(a, b):
+def div(a, b):
     c = a / b
     return c
 
 def square(a):
-    c = a**2
+    c = a * a
     return c
 
-def squareroot(a):
-    c = a**(1/2)
+def squareRoot(a):
+    math.sqrt(a)
     return c
 
-
-class Calculator:
+class calculator:
     result = 0
 
     def __init__(self):
         pass
 
     def add(self, a, b):
-        self.result = addition(a, b)
+        self.result = add(a, b)
+        return  self.result
+
+    def sub(self, a, b):
+        self.result = sub(a, b)
+        return  self.result
+
+    def times(self, a, b):
+        self.result = times(a, b)
+        return  self.result
+
+    def div(self, a, b):
+        self.result = div(a, b)
         return self.result
-    def subtract(self, a, b):
-        self.result = subtraction(a, b)
-        return self.result
-    def multiplication(self, a, b):
-        self.result = multiplication(a, b)
-        return self.result
-    def division(self, a, b):
-        self.result = division(a, b)
-        return self.result
+
     def square(self, a):
         self.result = square(a)
-        return self.result
-    def squareroot(self, a):
-        self.result = squareroot(a)
-        return self.result
+
+    def sqrt(self, a):
+        self.result = math.sqrt(a)
